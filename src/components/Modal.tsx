3539bf3641f9c5ca5010345b50866c2cfff1c372
patrick333c <jdmalaluan2@gmail.com>
2025-03-14 15:49:36 +0800

    commit

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import QRCodeButton from "./QRCodeButton";
 
 type ModalProps = {
   date: string; // Expected format: YYYY-MM-DD
