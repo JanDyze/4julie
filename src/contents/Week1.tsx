f5c6ca238fbad9a8352e23ea514f0cd872ce7aaf
patrick333c <jdmalaluan2@gmail.com>
2025-03-16 23:13:33 +0800

    jj

--- src/contents/Week1.tsx
+++ src/contents/Week1.tsx
@@ -12,7 +12,7 @@ const Week1 = () => {
 
     return (
         <>
-            <Modal date="2025-03-17" redirectTo="/" />
+            <Modal date="2025-03-18" redirectTo="/" />
             {showPoem && (
                 <Poem
                     title="Iba Na Ang 'Yong Dalampasigan"
